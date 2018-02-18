202a2a4ac341387f27985969b5a2fab87a838356
Yingbo (Max) Wang <maxwyb@gmail.com>
2018-02-17 21:24:35 -0800

    Add Houndify server-side npm module. Add in JavaScript directly in HTML to take user microphone input and give back response, mostly callback functions.

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
+var houndifyExpress = require('houndify').HoundifyExpress;
+app.get('/textSearchProxy', houndifyExpress.createTextProxyHandler());
+
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
