3b4747123f4482e3b0870e3cf9fe6b96616f268c
Yingbo (Max) Wang <maxwyb@gmail.com>
2018-02-17 22:11:09 -0800

    Add Houndify transcription to knockout.js binding for live update. Move Houndify codes to new script file.

--- public/scripts/AppViewModel.js
+++ public/scripts/AppViewModel.js
@@ -1,5 +1,6 @@
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
+    /*
     this.firstName = ko.observable("Bert");
     this.lastName = ko.observable("Bertington");
     this.fullName = ko.computed(function() {
@@ -9,6 +10,22 @@ function AppViewModel() {
     this.capitalizeLastName = function() {
         var currentVal = this.lastName();
         this.lastName(currentVal.toUpperCase());
+    }
+    */
+    this.transcriptionString = window.transcriptionString;
+
+    this.onMicrophoneClick = function () {
+        if (!recorder) {
+            return;
+        }
+
+        if (recorder.isRecording()) {
+            recorder.stop();
+            console.log("Microphone button clicked, stop recorder.");
+        } else {
+            recorder.start();
+            console.log("Microphone button clicked, start recorder");
+        }
     }
 }
 
---
+++ public/scripts/microphone.js
@@ -0,0 +1,100 @@
+// Global parameters used by other parts of the app
+var transcriptionString = ko.observable();
+
+// Houndify parameter initializations
+var clientId = "5EPSnXvMIUpGezVkC3QUGw==";
+var clientKey = "UQVjbSfJSGUXMnEoixSwZHDa5TrpwE1t-_z3KvqARkhMqDabq8qKA2EP0ibFsqU2UxD7Ue3rtVy5LQRX9GA3zg==";
+var conversationState = null;
+var voiceRequest = null;
+
+// Houndify recorder setup
+var recorder = new Houndify.AudioRecorder();
+recorder.on('start', function() {
+    console.log("recorder.start");
+    initVoiceRequest(recorder.sampleRate);
+});
+
+recorder.on('data', function(data) {
+    console.log("recorder.data");
+    voiceRequest.write(data);
+});
+
+recorder.on('end', function() {
+    console.log("recorder.end");
+    voiceRequest.end();
+})
+
+recorder.on('error', function(error) {
+    console.log("recorder.error");
+    voiceRequest.abort();
+})
+
+// callback functions for receiving Houndify API responses
+function onResponse(response, info) {
+    if (response.AllResults && response.AllResults.length) {
+        console.log("onResponse: ");
+        console.log(response.AllResults);
+        conversationState = response.AllResults[0].ConversationState;
+    }
+}
+
+function onError(err, info) {
+    console.log("onError: ");
+    console.log(err, info);
+}
+
+function onTranscriptionUpdate(transcript) {
+    console.log("onTranscriptionUpdate:");
+    console.log(transcript.PartialTranscript);
+    transcriptionString(transcript.PartialTranscript);
+}
+
+// event-handler functions
+function initVoiceRequest(sampleRate) {
+    voiceRequest = new Houndify.VoiceRequest({
+        //Your Houndify Client ID
+        clientId: "5EPSnXvMIUpGezVkC3QUGw==",
+
+        //For testing environment you might want to authenticate on frontend without Node.js server. 
+        //In that case you may pass in your Houndify Client Key instead of "authURL".
+        clientKey: "UQVjbSfJSGUXMnEoixSwZHDa5TrpwE1t-_z3KvqARkhMqDabq8qKA2EP0ibFsqU2UxD7Ue3rtVy5LQRX9GA3zg==",
+
+        //Otherwise you need to create an endpoint on your server
+        //for handling the authentication.
+        //See SDK's server-side method HoundifyExpress.createAuthenticationHandler().
+        //authURL: "/houndifyAuth",
+
+        //REQUEST INFO JSON
+        //See https://houndify.com/reference/RequestInfo
+        requestInfo: {
+            UserID: "web_test_user",
+            Latitude: 37.428,
+            Longitude: -122.174
+        },
+
+        //Pass the current ConversationState stored from previous queries
+        //See https://www.houndify.com/docs#conversation-state
+        conversationState: conversationState,
+
+        //Sample rate of input audio
+        sampleRate: sampleRate,
+
+        //Enable Voice Activity Detection
+        //Default: true
+        enableVAD: true,
+
+        //Partial transcript, response and error handlers
+        onTranscriptionUpdate: onTranscriptionUpdate,
+
+        onResponse: function (response, info) {
+            recorder.stop();
+            onResponse(response, info);
+        },
+        onError: function (err, info) {
+            recorder.stop();
+            onError(err, info);
+        }
+    });
+
+    return voiceRequest;
+}
