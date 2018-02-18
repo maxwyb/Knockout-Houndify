0f1e81ce3d57499a45c4972f60a13c0d9b908916
Yingbo (Max) Wang <maxwyb@gmail.com>
2018-02-18 02:39:24 -0800

    Add heroku deployment port configuration and starting script.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var path = require('path')
 var express = require('express')
 var app = express()
+const PORT = process.env.PORT || 5000  // heroku deployment
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
@@ -12,4 +13,4 @@ var houndifyExpress = require('houndify').HoundifyExpress;
 app.get('/textSearchProxy', houndifyExpress.createTextProxyHandler());
 
 app.use(express.static('public'))
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log('Example app listening on port 3000!'))
